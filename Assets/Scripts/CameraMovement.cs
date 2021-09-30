fd388cfa7faeab7b65e8203eae1c95af7223637b
Isaiah Noah Santos <isaiahnoah.santos@ontariotechu.net>
2021-09-30 11:49:25 -0400

    Added UML Diagram png

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,11 +6,6 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public Vector3 offSet;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
